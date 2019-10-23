c09fe6c6eb178238c7521e34d7ec9a899e6225a2
Tom Kwong <tk3369@gmail.com>
2019-10-22 23:15:22 -0700

    Files generated by PkgTemplates

---
+++ src/MonadFunctions.jl
@@ -0,0 +1,5 @@
+module MonadFunctions
+
+greet() = print("Hello World!")
+
+end # module
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MonadFunctions
+using Test
+
+@testset "MonadFunctions.jl" begin
+    # Write your own tests here.
+end
